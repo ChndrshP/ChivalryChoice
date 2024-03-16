fce68902813566e04c34481733376ddbf7408dfc
Chandresh <chndrsh25@gmail.com>
2024-03-16 23:08:51 +0530

    changes made

--- server/utils/getTokenFromHeader.js
+++ server/utils/getTokenFromHeader.js
@@ -1,10 +1,10 @@
-export const getTokenFromHeader = (req)=>{
- //get token from header
- const token = req?.headers.authorization.split(" ")[1];
+   export const getTokenFromHeader = (req)=>{
+   //get token from header
+   const token = req?.headers.authorization?.split(" ")[1] || 'No token found in the header';
 
- if(token == undefined){
-    return 'No token found in the header'
- }else{
-    return token;
- }
-}
+   if(token == undefined){
+      return 'No token found in the header'
+   }else{
+      return token;
+   }
+   }

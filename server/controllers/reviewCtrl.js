c356caaf9282b73a86f58411066c484b77782647
Chandresh <chndrsh25@gmail.com>
2024-06-12 14:41:56 +0530

    Review created

--- server/controllers/reviewCtrl.js
+++ server/controllers/reviewCtrl.js
@@ -7,11 +7,29 @@ import Product from "../model/Product.js";
 // @access Private/Admin
 
 export const createReviewCtrl = asyncHandler(async(req, res) =>{
-    //Find the product
     const {product, message, rating} = req.body;
+    //Find the product
+    const {productID} = req.params;
     const productFound = await Product.findById(productID);
     if(!productFound){
         throw new Error("Product not Found");
     }
     //If user already reviewed this product
+
+    //create review
+    const review = await Review.create({
+        message,
+        rating, 
+        product: productFound?._id,
+        user: req.userAuthId,
+    });
+
+    //Push Review into Product found
+    productFound.reviews.push(review?._id)
+    //resave
+    await productFound.save();
+    res.status(201).json({
+        success: true,
+        message: "Review created successfully",
+    });
 }) 

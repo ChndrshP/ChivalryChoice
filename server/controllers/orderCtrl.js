1f1281f4bde531bdd5a2896f5f9897a00fdc6cf2
Chandresh <chndrsh25@gmail.com>
2024-07-03 15:21:22 +0530

    Stripe Payment controller - I

--- server/controllers/orderCtrl.js
+++ server/controllers/orderCtrl.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import Stripe from "stripe"; 
 import Order from "../model/Order.js";
 import User from "../model/User.js";
 import Product from "../model/Product.js";
@@ -6,6 +7,9 @@ import Product from "../model/Product.js";
 // @desc Create order
 // @route POST /api/orders
 // @access Private
+
+//stripe
+const stripe = new Stripe(process.env.STRIPE_KEY);
 
 export const createOrderCtrl = asyncHandler(async (req, res) => {
 
@@ -50,14 +54,31 @@ export const createOrderCtrl = asyncHandler(async (req, res) => {
     await user?.save();
 
     //make pamyment (stripe)
-
+    const session = await stripe.checkout.sessions.create({
+        line_items :[{
+            pricee_data:{
+                currency:"usd",
+                product_data:{
+                    name:'Hats',
+                    description: "Best Hat",
+                },
+                unit_amount: 10*100,
+            },
+            quantity: 2,
+            },
+        ],
+        mode:"payment",
+        success_url:'http://localhost:3000/success',
+        cancel_url:'http://localhost:3000/success/cancel'
+    });
+    res.send({url: session.url});
     //Payment webhook
 
     //Update the user order 
-    res.json({
-        success: true,
-        message: "Order created successfully",
-        order,
-        user,
-    })
+    // res.json({
+    //     success: true,
+    //     message: "Order created successfully",
+    //     order,
+    //     user,
+    // })
 });

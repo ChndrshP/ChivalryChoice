5066deff4666e368a997ef73bbd6116f02fd605a
Chandresh <chndrsh25@gmail.com>
2024-07-05 16:26:39 +0530

    feat: Implement Stripe webhook for handling payment events
    - Added Stripe webhook endpoint to handle various payment events such as payment_intent.succeeded, payment_intent.payment_failed, and charge.refunded.
    - Verified signature of incoming webhook events for security.

--- server/app/app.js
+++ server/app/app.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import Stripe from "stripe"; 
 dotenv.config();
 import express from 'express'
 import dbConnect from '../config/dbConnect.js'
@@ -15,6 +16,42 @@ import orderRouter from '../routes/ordersRouter.js';
 //dbConnect
 dbConnect();
 const app = express();
+
+//stripe Webhook
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_05555e06eaa2cdffb7b2397f6e835149f1bdc72e6957be800ee58aa500765f30";
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    console.log("event");
+  } catch (err) {
+    console.log('err', err.message);
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
 
 //pass incoming data
 app.use(express.json());

166028509f0e876c2e39085a95b05bdbee6ad30a
Chandresh <chndrsh25@gmail.com>
2024-07-14 20:39:31 +0530

    Coupon Router - all functions added

--- server/controllers/couponCtrl.js
+++ server/controllers/couponCtrl.js
@@ -47,3 +47,52 @@ export const getAllCouponsCtrl = asyncHandler(async (req, res) => {
         coupons,
     });
 });
+
+//@desc   Get single Coupon
+//@route  GET /api/v1/coupons/:id
+//@access Private/Admin
+
+export const getSingleCouponCtrl = asyncHandler(async (req, res) => {
+    const coupon = await Coupon.findById(req.params.id);
+    if(!coupon){
+        throw new Error("Coupon not found");
+    }
+    res.status(200).json({
+        status: "success",
+        message: "Coupon found",
+        coupon,
+    });
+});
+
+//@desc   Update Coupon
+//@route  GET /api/v1/coupons/:id
+//@access Private/Admin
+
+export const updateCouponCtrl = asyncHandler(async (req, res) => {
+    const coupon = await Coupon.findByIdAndUpdate(req.params.id, {
+        code: code?.toUpperCase(),
+        discount,
+        startDate,
+        endDate,
+    },{
+        new: true,    
+    });
+    res.json({
+        status: "success",
+        message: "Coupon updated successfully",
+        coupon,
+    });
+});
+
+//@desc   Delete Coupon
+//@route  GET /api/v1/coupons/:id
+//@access Private/Admin
+
+export const deleteCouponCtrl = asyncHandler(async (req, res) => {
+    const coupon = await Coupon.findByIdAndDelete(req.params.id)
+    res.json({
+        status: "success",
+        message: "Coupon deleted successfully",
+        coupon,
+    });
+});
--- server/routes/couponRouter.js
+++ server/routes/couponRouter.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
 import { createCouponCtrl,
-        getAllCouponsCtrl
+        getAllCouponsCtrl,
+        getSingleCouponCtrl,
+        updateCouponCtrl,
+        deleteCouponCtrl
         } from '../controllers/couponCtrl.js';
 
 const couponRouter = express.Router();
 
 couponRouter.post('/', isLoggedIn, createCouponCtrl);
 couponRouter.get('/', getAllCouponsCtrl);
+couponRouter.get('/:id', getSingleCouponCtrl);
+couponRouter.put('/update/:id', isLoggedIn, updateCouponCtrl);
+couponRouter.delete('/delete/:id', isLoggedIn, deleteCouponCtrl);
 
 export default couponRouter;
  

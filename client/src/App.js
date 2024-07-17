685c543d03f315357db84e9d9e51977a96e04bb8
Chandresh <chndrsh25@gmail.com>
2024-07-17 18:24:02 +0530

    feat- crated the frontend template

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl text-red-500">Hey there!</h1>
+    </div>
+  );
+}
+
+export default App;

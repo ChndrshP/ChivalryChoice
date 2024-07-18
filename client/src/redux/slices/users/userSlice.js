00bc4bfb12272c468c07e913c937edd9fef6fd67
Chandresh <chndrsh25@gmail.com>
2024-07-18 14:42:31 +0530

    feat: Add base URL for API requests and Login

---
+++ client/src/redux/slices/users/userSlice.js
@@ -0,0 +1,34 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
+import baseURL from '../../../utils/baseURL';
+
+//initial state
+const initialState = {
+    loading: false,
+    errro: false,
+    users:[],
+    user:{},
+    profile:{},
+    userAuth:{
+        loading:false,
+        error:null,
+        userInfo: {},
+    },
+};
+
+//login action
+export const loginUserAction = createAsyncThunk(
+    "users/login",
+    async ({email, password}, { rejectWithValue, getState, dispatch }) => {
+        try {
+            //make the http request
+            const res = await axios.post(`${baseURL}/users/login`, {
+                email,
+                password,
+            });
+            return data;
+        } catch (error) {
+            return rejectWithValue(error?.response?.data);
+        }
+    }
+);

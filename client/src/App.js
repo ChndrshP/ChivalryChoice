52f9faf5d5c7d40941833248a2799b1dbc8ff59e
Chandresh <chndrsh25@gmail.com>
2024-07-19 23:34:27 +0530

    bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-2xl text-red-500">Hey there!</h1>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <h1 className="text-2xl text-red-500">Hey there!</h1>
+      </div>
+      <div>
+        <h1></h1>
+      </div>
+    </React.Fragment>
   );
 }
-
 export default App;

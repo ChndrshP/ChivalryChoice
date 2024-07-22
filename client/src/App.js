b7ae32432ae868df907a61a966bad42309eea93b
Chndrsh25 <chandresh2003cp@gmail.com>
2024-07-22 23:53:11 +0530

    chore: Remove unused code and update Redux store configuration

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,7 @@ function App() {
   return (
     <React.Fragment>
       <div className="App">
-        <h1 className="text-2xl text-red-500">Hey there!</h1>
-      </div>
-      <div>
-        <h1></h1>
+        
       </div>
     </React.Fragment>
   );
